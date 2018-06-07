96ad4ce68a84ff1b5156a2b05e6cb5af514ce097
rebe4175 <rebe4175@kea.stud.dk>
2018-06-07 17:48:58 +0200

    skelet

---
+++ sekvens.js
@@ -0,0 +1,24 @@
+$(window).on("load", sidenVises);
+
+function sidenVises() {
+
+    console.log("Siden Vises");
+
+    $(".menubar").on("click", openNav);
+    $(".closebtn").on("click", closeNav)
+}
+
+function openNav() {
+
+    console.log("Nav Opens");
+
+    $("#sidenavbar").addClass("open");
+
+}
+
+function closeNav() {
+
+    console.log("Nav Closes");
+
+    $("#sidenavbar").removeClass("open");
+}
